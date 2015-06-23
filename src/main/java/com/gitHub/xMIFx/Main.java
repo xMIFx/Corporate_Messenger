c461fd7879d6c6b5cfcfa09c8c1b67bb13e16eff
Vladislav <bukatinvladislav@gmail.com>
2015-06-23 21:26:07 +0300

    beginning

---
+++ src/main/java/com/gitHub/xMIFx/Main.java
@@ -0,0 +1,10 @@
+package com.gitHub.xMIFx;
+
+/**
+ * Created by Vlad on 23.06.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/gitHub/xMIFx/entity/Department.java
@@ -0,0 +1,7 @@
+package com.gitHub.xMIFx.entity;
+
+/**
+ * Created by Vlad on 23.06.2015.
+ */
+public class Department {
+}
